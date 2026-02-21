5669d4ffdb54273369508583ec46a675de9e9cff
Vetrivel <vetrisurabi@gmail.com>
2026-02-21 14:40:51 +0530

    rename appropriate packages

--- matching-engine/src/main/java/org/trading/engine/Main.java
+++
@@ -1,7 +0,0 @@
-package org.trading.engine;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Running");
-    }
-}
---
+++ matching-engine/src/main/java/org/trading/exchange/Main.java
@@ -0,0 +1,7 @@
+package org.trading.exchange;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exchange Running...");
+    }
+}
