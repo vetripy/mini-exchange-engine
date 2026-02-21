66e4a0692c31344e1b23eb6451650ee75a8352f6
Vetrivel <vetrisurabi@gmail.com>
2026-02-21 15:09:10 +0530

    add Trade model

---
+++ matching-engine/src/main/java/org/trading/exchange/model/Trade.java
@@ -0,0 +1,27 @@
+package org.trading.exchange.model;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@ToString
+public class Trade {
+    private final String id;
+    private final String buyOrderId;
+    private final String sellOrderId;
+    private final long price;
+    private final long quantity;
+    private final Instant timestamp;
+
+    public Trade(String id, String buyOrderId, String sellOrderId, long price, long quantity, Instant timestamp) {
+        this.id = UUID.randomUUID().toString();
+        this.buyOrderId = buyOrderId;
+        this.sellOrderId = sellOrderId;
+        this.price = price;
+        this.quantity = quantity;
+        this.timestamp = Instant.now();
+    }
+}
