7147c034b6c9a1979506251002583936711be965
Vetrivel <vetrisurabi@gmail.com>
2026-02-21 22:42:58 +0530

    add unit tests for cancel order

--- matching-engine/src/test/java/org/trading/exchange/orderbook/OrderBookTest.java
+++ matching-engine/src/test/java/org/trading/exchange/orderbook/OrderBookTest.java
@@ -55,7 +55,7 @@ public class OrderBookTest {
         orderBook.addOrder(getValidBuyOrderWith(10L, 3L));
 
         orderBook.printDepth();
-        
+
         //Then
         assertEquals(0, orderBook.getBuySnapshot().size());
         assertEquals(1, orderBook.getSellSnapshot().size());
@@ -85,6 +85,61 @@ public class OrderBookTest {
         assertEquals(1L, orderBook.getBuySnapshot().size());
         assertEquals(0L, orderBook.getSellSnapshot().size());
         assertEquals(5L, orderBook.getBuySnapshot().get(10L).getFirst().getRemainingQuantity());
+    }
+
+    @Test
+    @DisplayName("Test cancelling an order in the order book")
+    void testCancelOrder() {
+        //Given
+        Order buyOrder = getValidBuyOrderWith(10L, 10L);
+        orderBook.addOrder(buyOrder);
+        orderBook.cancelOrder(buyOrder.getId());
+
+        //Then
+        assertEquals(0, orderBook.getBuySnapshot().size());
+    }
+
+    @Test
+    @DisplayName("Test cancelling orders that have been partially filled in the order book")
+    void testCancelPartiallyFilledOrder() {
+        //Given
+        Order buyOrder = getValidBuyOrderWith(10L, 10L);
+        orderBook.addOrder(buyOrder);
+        orderBook.addOrder(getValidSellOrderWith(10L, 5L));
+        orderBook.cancelOrder(buyOrder.getId());
+
+        //Then
+        assertEquals(0, orderBook.getBuySnapshot().size());
+    }
+
+    @Test
+    @DisplayName("Test cancelling an order that has already been fully filled in the order book")
+    void testCancelFullyFilledOrder() {
+        //Given
+        Order buyOrder = getValidBuyOrderWith(10L, 10L);
+        orderBook.addOrder(buyOrder);
+        orderBook.addOrder(getValidSellOrderWith(10L, 10L));
+
+        //Then
+        try {
+            orderBook.cancelOrder(buyOrder.getId());
+        } catch (IllegalArgumentException e) {
+            assertEquals("Order not found: " + buyOrder.getId(), e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Test cancelling a non-existent order in the order book")
+    void testCancelNonExistentOrder() {
+        //Given
+        String nonExistentOrderId = UUID.randomUUID().toString();
+
+        //Then
+        try {
+            orderBook.cancelOrder(nonExistentOrderId);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Order not found: " + nonExistentOrderId, e.getMessage());
+        }
     }
 
 
