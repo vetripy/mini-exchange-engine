76ac737ad05c1a672aad5551bad5871e30fef8ea
Vetrivel <vetrisurabi@gmail.com>
2026-02-26 21:05:48 +0530

    add OrderUpdateListener and logging implementation

---
+++ matching-engine/src/main/java/org/trading/exchange/listener/impl/LoggingOrderUpdateListener.java
@@ -0,0 +1,17 @@
+package org.trading.exchange.listener.impl;
+
+import org.trading.exchange.listener.OrderUpdateListener;
+import org.trading.exchange.model.OrderUpdate;
+
+public class LoggingOrderUpdateListener implements OrderUpdateListener {
+
+    @Override
+    public void onOrderUpdate(OrderUpdate update) {
+        System.out.println("ORDER UPDATE → "
+                + update.getOrderId()
+                + " "
+                + update.getOrderState()
+                + " remaining: "
+                + update.getRemainingQuantity());
+    }
+}
