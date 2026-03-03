632e3c286073fc4fa78d77cf94a5b1c354217dfb
Vetrivel <vetrisurabi@gmail.com>
2026-03-03 13:53:11 +0530

    add EngineEvent, with type

--- matching-engine/src/main/java/org/trading/exchange/event/EngineEvent.java
+++ matching-engine/src/main/java/org/trading/exchange/event/EngineEvent.java
@@ -1,4 +1,18 @@
 package org.trading.exchange.event;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public class EngineEvent {
+    private final long sequenceNumber;
+    private final Type type;
+    private final Object data;
+
+    public enum Type {
+        TRADE,
+        ORDER_UPDATE
+    }
+
 }
