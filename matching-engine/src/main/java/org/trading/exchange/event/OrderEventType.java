7e991c70d98ebe0fec0bce781dcb96d700d2ddf0
Vetrivel <vetrisurabi@gmail.com>
2026-03-26 08:29:12 +0530

    refactor enums to be in model package

--- matching-engine/src/main/java/org/trading/exchange/event/OrderEventType.java
+++
@@ -1,6 +0,0 @@
-package org.trading.exchange.event;
-
-public enum OrderEventType {
-  NEW_ORDER,
-  CANCEL_ORDER
-}
---
+++ matching-engine/src/main/java/org/trading/exchange/model/OrderEventType.java
@@ -0,0 +1,6 @@
+package org.trading.exchange.model;
+
+public enum OrderEventType {
+    NEW_ORDER,
+    CANCEL_ORDER
+}
