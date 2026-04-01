8c2d89dc396e502cc002c2209832f8e8bc82dbac
Vetrivel <vetrisurabi@gmail.com>
2026-04-01 09:03:42 +0530

    apply spotless

--- matching-engine/src/test/java/org/trading/exchange/engine/MatchingEngineTest.java
+++ matching-engine/src/test/java/org/trading/exchange/engine/MatchingEngineTest.java
@@ -96,9 +96,9 @@ class MatchingEngineTest {
         assertEquals(100L, tradeEvent.getTradePrice());
         assertEquals(5L, tradeEvent.getQuantity());
         assertEquals(((NewOrderCommand) buyCommand).getClientOrderId(),
-            tradeEvent.getBuyClientOrderId());
+                tradeEvent.getBuyClientOrderId());
         assertEquals(((NewOrderCommand) sellCommand).getClientOrderId(),
-            tradeEvent.getSellClientOrderId());
+                tradeEvent.getSellClientOrderId());
     }
 
     @Test
@@ -127,10 +127,10 @@ class MatchingEngineTest {
         assertEquals(4L, tradeEvent.getQuantity());
 
         OrderUpdateEvent lastUpdate =
-            orderUpdateListener.getUpdates().stream()
-                .filter(orderUpdate -> Objects.equals(orderUpdate.getClientOrderId(),
-                    ((NewOrderCommand) buyCommand).getClientOrderId()))
-                .toList().getLast();
+                orderUpdateListener.getUpdates().stream()
+                        .filter(orderUpdate -> Objects.equals(orderUpdate.getClientOrderId(),
+                                ((NewOrderCommand) buyCommand).getClientOrderId()))
+                        .toList().getLast();
 
         assertEquals(OrderState.PARTIALLY_FILLED, lastUpdate.getOrderState());
         assertEquals(6L, lastUpdate.getRemainingQuantity());
@@ -181,7 +181,7 @@ class MatchingEngineTest {
         assertEquals(2, tradeListener.getTrades().size());
 
         long totalQuantity =
-            tradeListener.getTrades().stream().mapToLong(TradeEvent::getQuantity).sum();
+                tradeListener.getTrades().stream().mapToLong(TradeEvent::getQuantity).sum();
 
         assertEquals(5L, totalQuantity);
     }
@@ -193,7 +193,7 @@ class MatchingEngineTest {
         engine.submit(buyCommand);
 
         assertThrows(IllegalArgumentException.class,
-            () -> engine.submit(CancelOrderCommand.of("unknown-client-id")));
+                () -> engine.submit(CancelOrderCommand.of("unknown-client-id")));
     }
 
     @Test
